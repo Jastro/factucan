b28c67a43d203e8ecd2c87a1f68e2565edd1ddbe
Jacobo Caraballo <jacobo.caraballo@gmail.com>
2021-03-08 17:12:05 +0100

    Basic login core

---
+++ src/mockup/data.js
@@ -0,0 +1,40 @@
+export const CreateAccountInputs = [
+  {
+    placeholder: "First Name *",
+    label: "First Name (middle name) *",
+    error: "Enter your first name",
+    haveError: false,
+    isRequired: true,
+  },
+  {
+    placeholder: "Last Name *",
+    label: "Last Name *",
+    error: "Enter your family name",
+    haveError: false,
+    isRequired: true,
+  },
+  {
+    placeholder: "myemail@example.com *",
+    label: "Email *",
+    error: "Enter your email",
+    haveError: false,
+    isRequired: true,
+    type: "email",
+  },
+  {
+    placeholder: "Choose password *",
+    label: "Password *",
+    error: "Enter your password",
+    haveError: false,
+    isRequired: true,
+    type: "pass",
+  },
+  {
+    placeholder: "Repeat password *",
+    label: "Password *",
+    error: "Repeat your password",
+    haveError: false,
+    isRequired: true,
+    type: "pass",
+  },
+];
